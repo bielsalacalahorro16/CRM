0b8e68d5fad28dfda36027b6a8039d7fe1987871
biel sala calahorro <biel.sala@MacBook-Pro-de-biel.local>
2022-01-15 00:03:47 +0100

    Add activation of plugins as middleware

--- Main/main.ts
+++ Main/main.ts
@@ -4,14 +4,14 @@ interface IModuleResolver {
   app: Application;
   plugins: string[];
   getPlugins(): Promise<void>;
-  activatePlugins(app: Application) : void;
+  activatePlugins(app: Application): void;
 }
 
 class ModuleResolver implements IModuleResolver {
   plugins: string[];
   app: Application;
 
-  constructor(app: Application){
+  constructor(app: Application) {
     this.app = app;
     this.plugins = [];
   }
@@ -19,19 +19,19 @@ class ModuleResolver implements IModuleResolver {
     const decoder: TextDecoder = new TextDecoder("utf-8");
     const data: Uint8Array = await Deno.readFile("plugins.json");
     const parsedData = JSON.parse(decoder.decode(data));
- 
-    if(parsedData.hasOwnProperty('plugins') ) {
+
+    if (parsedData.hasOwnProperty("plugins")) {
       this.plugins = parsedData.plugins;
     }
 
     this.activatePlugins(this.app);
   }
 
-  activatePlugins(app: Application): void {
-    for(let plugin of this.plugins){
-      //Import all the apps from the diff plugins;
+  async activatePlugins(app: Application): Promise<void> {
+    for (let plugin of this.plugins) {
+      //TODO: Dynamic path with module name, error handling
+      let { app } = await import("../Main/test.ts");
       app.use(app);
     }
   }
 }
-
---
+++ Main/test.ts
@@ -0,0 +1,4 @@
+//This is a test file only for developing purpouses. 
+import { opine } from "https://deno.land/x/opine@2.1.1/mod.ts";
+const app = opine();
+export {app}
