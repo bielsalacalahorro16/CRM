646086f5f8efc124a408a7ae4c249db8dab0d677
biel sala calahorro <bielwork16@gmail.com>
2022-01-17 21:46:27 +0100

    testing unmount routes

--- Plugins/article/article.ts
+++ Plugins/article/article.ts
@@ -1,4 +1,20 @@
 //This is a test file only for developing purpouses. 
 import { opine } from "https://deno.land/x/opine@2.1.1/mod.ts";
+import { Router } from "https://deno.land/x/opine@2.1.1/mod.ts";
+
 const app = opine();
+
+const router = Router();
+router.get("/events", function (req, res, next) {
+  // ..
+});
+router.get("/biel/:id", function (req, res, next) {
+  // ..
+});
+router.get("/biel/", function (req, res, next) {
+  // ..
+});
+app.use("/calendar", router);
+//router.stack.filter(el => console.log(el.route.path))
+console.log(app._router)
 export {app}
