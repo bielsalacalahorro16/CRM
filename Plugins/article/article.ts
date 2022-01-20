132efcafb931e87ae37d5e568deedc4b76150ac7
bielsalacalahorro16 <bielwork16@gmail.com>
2022-01-20 23:12:29 +0100

    correct import

--- Plugins/article/article.ts
+++ Plugins/article/article.ts
@@ -1,5 +1,5 @@
-//This is a test file only for developing purpouses. 
-import { opine } from "https://deno.land/x/opine@2.1.1/mod.ts";
+//This is a test file only for developing purpouses.
+/*import { opine } from "https://deno.land/x/opine@2.1.1/mod.ts";
 import { Router } from "https://deno.land/x/opine@2.1.1/mod.ts";
 
 const app = opine();
@@ -17,4 +17,5 @@ router.get("/biel/", function (req, res, next) {
 app.use("/calendar", router);
 //router.stack.filter(el => console.log(el.route.path))
 console.log(app._router)
-export {app}
+export {app}*/
+
